8cf7082c9931865722de220e9d72ac37cba4dd97
ArnavSingha <102987879+ArnavSingha@users.noreply.github.com>
2024-01-17 22:57:05 +0530

    easy!!

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+// const a = 5654;
+
+// alert(a);
+
+// -----------------------------------------------
+// let a = 25;
+// let namee ="Arnav";
+
+// console.log(a);
+// console.log(namee);
+
+// ------------------------------------------------------------------
+// let a:string = "562";
+// let num:number = 56235;
+// let check:boolean;
+
+// check=true;
+
+// ----------------------------------------------------
+
+// let a;
+// let num;                    this is the any type Try to avoid that in TS
+// let check;
+
+
+// --------------------------------------------------
+
+// let num=<string>566;
+//  and this
+// let num:string='477848';           this are the normal var
+
+
+// let surname:string|number;
+// surname= '5622';                              this is called union type
+
+
+
